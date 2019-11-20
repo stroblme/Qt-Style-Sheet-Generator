346c34a7dfc3b2b55321f93e8cb9b4bf130fc55c
m17538 <melvin.strobl@microchip.com>
2019-11-20 18:12:30 +0100

    updated readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Style Sheet Generator
+
+This software generates a QT style sheet based on the following two software components:
+- Paletton (https://paletton.com/)
+- Breeze Style Sheet (https://github.com/Alexhuszagh/BreezeStyleSheets)
+
+From Paletton, you can define a 3-color based template and export it as a .txt file.
+This then can be merged either with one of the by SSG provided templates:
+- dark.template
+- light.template
+
+Last but not least, you have to specify an output directory, where your custom QSS file will be written to.
+
+This file then can be used for creating a QT conform asset.
--- ssg.py
+++ ssg.py
@@ -1,14 +1,55 @@
 import sys
 import os
+import argparse
 
 from paletteParser import PaletteParser
 from generator import Generator
 
 PALETTEFILENAME = "./palette.txt"
-TEMPLATEFILENAME = "./breeze/dark.template"
+TEMPLATEFILENAME = "./breeze.template"
 OUTPUTFILENAME = "./output/dark.qss"
 
+def argumentHelper():
+    """
+    Just a helper for processing the arguments
+    """
+
+    # Define Help Te
+    helpText = 'Register Converter Script'
+    # Create ArgumentParser instance
+    argparser = argparse.ArgumentParser(description=helpText)
+
+    
+    argparser.add_argument('-p', '--palette',
+                        help='Your color palette')
+    argparser.add_argument('-t', '--template',
+                        help='Template file')
+    argparser.add_argument('-o', '--output',
+                        help='Output file')
+
+    return argparser.parse_args()
+
 def main():
+    global PALETTEFILENAME, TEMPLATEFILENAME, OUTPUTFILENAME
+
+    #-----------------------------------------------------
+    # ---------------Argument processing------------------
+    args = None
+    try:
+        args = argumentHelper()
+    except ValueError as e:
+        print("Unable to parse arguments:\n")
+        sys.exit(str(e.args))
+
+    if args.palette:
+        PALETTEFILENAME = args.palette
+
+    if args.template:
+        TEMPLATEFILENAME = args.template
+        
+    if args.output:
+        OUTPUTFILENAME = args.output
+        
     paletteParserInst = PaletteParser()
 
     paletteParserInst.parseDocument(PALETTEFILENAME)
