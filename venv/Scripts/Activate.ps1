1af57df87f84b820508e1474b4b0b69da2e9c160
Melvin Strobl <melvin.strobl@web.de>
2019-11-21 23:02:41 +0100

    template

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\m17538\Projects\styleSheetGenerator\venv"
+$env:VIRTUAL_ENV="X:\SSG - Style Sheet Generator\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
